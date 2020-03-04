3e365c8cae9193da096d4552ae0bc82dddce07a8
JeazChen <JeazChen@163.com>
2020-03-04 16:57:41 +0800

    ajax city

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,19 +1,15 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
+    <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'CityAlphabet'
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  }
 }
 </script>
 
@@ -29,7 +25,7 @@ export default {
   bottom: 0;
   width: .4rem;
   .item{
-    line-height: .44rem;
+    line-height: .4rem;
     text-align: center;
     color: @bgColor;
   }
